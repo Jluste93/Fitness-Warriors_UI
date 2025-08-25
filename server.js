45bc17516f1ef660c3b1a516cf2f1eefa20ab540
Jluste93 <164100840+Jluste93@users.noreply.github.com>
2025-08-24 22:48:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ app.patch('/schedule/:id/reschedule', async (req, res) => {
 // Catch-all route
 //app.get('*', (req, res) => {
 //    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-//});
+//});..
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
